609a9131b0562bfdc292aa6f29adafd9a08b8ba1
hanzjk <hansi@wso2.com>
2026-01-15 15:06:13 +0530

    Remove HAR Demo provider from dataset providers to simplify module exports

--- providers/__init__.py
+++ providers/__init__.py
@@ -1,6 +1,5 @@
 """Dataset Providers"""
 
-from .har_demo import HARDemoProvider
 from .common import FeatureExtractorUtils
 
-__all__ = ['HARDemoProvider', 'FeatureExtractorUtils']
+__all__ = ['FeatureExtractorUtils']
